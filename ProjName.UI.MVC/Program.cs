f65ebe253455fc5822c7a42c46290267361b4047
Jacquelyn525 <JacquelynLittle1@outlook.com>
2022-10-08 13:55:28 -0500

    Added Entity Framework

--- ProjName.UI.MVC/Program.cs
+++ ProjName.UI.MVC/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using projName.DATA.EF.UIModels;
 using ProjName.UI.MVC.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<StoreFrontContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
