de06dea838c9ace73dac56d6e80a808fe6c69be8
Jacquelyn525 <JacquelynLittle1@outlook.com>
2022-10-08 22:31:01 -0500

    Data Annotations

---
+++ projName.DATA.EF/Metadata/Metadata.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projName.DATA.EF//.Metadata
+{
+    public class CategoryMetadata
+    {
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "*Name is required")]
+        [StringLength(50, ErrorMessage = "*Must be 50 characters")]
+        [Display(Name = "Category")]
+        public string CategoryName { get; set; } = null!;
+
+    }
+    public class OrderMetadata
+    {
+        public int OrderId { get; set; }
+
+        [Required(ErrorMessage = "*Must enter a Quantity")]
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "*Order Date is required")]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+        [Display(Name = " Order Date")]
+        public DateTime OrderDate { get; set; }
+
+    }
+    public class ProductMetadata
+    {
+        public int ProductId { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "*Name is required")]
+        [StringLength(50, ErrorMessage = "*Cannot exceed 50 characters")]
+        [Display(Name = "Name?")]
+        public string ProductName { get; set; } = null!;
+
+        [Display(Name = "Quantity Per Unit")]
+        public int QtyPerUnit { get; set; }
+
+        [Display(Name = "Price")]
+        [Required(ErrorMessage = "*Price is required")]
+        [DisplayFormat(DataFormatString = "{0:c}", ApplyFormatInEditMode = false)]
+        [Range(0, (double)decimal.MaxValue)]
+        public decimal ProductPrice { get; set; }
+
+
+        public int ProductStatusId { get; set; }
+
+        [StringLength(500, ErrorMessage = "*Cannot exceed 500 characters")]
+        [Display(Name = "Description")]
+        [DataType(DataType.MultilineText)]
+        public string? ProductDescription { get; set; }
+
+    }
+    
+}
